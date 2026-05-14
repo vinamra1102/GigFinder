a95ffb73ff45eb7dc035a190a3c6abb458d07d36
vinamra1102 <vinamra.229302343@muj.manipal.edu>
2026-05-14 12:46:33 +0530

    Add db/database.py with SQLite engine and session factory

---
+++ db/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+DATABASE_URL = "sqlite:///gigfinder.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()

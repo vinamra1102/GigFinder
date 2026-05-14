35d3780982c45e6fbdaf3f086f0975123bc638ba
vinamra1102 <vinamra.229302343@muj.manipal.edu>
2026-05-14 12:48:26 +0530

    Add update_lead_status() with auto contacted_at and follow_up_due on contact

--- db/database.py
+++ db/database.py
@@ -42,3 +42,21 @@ def get_all_leads(status=None, subreddit=None, date_from=None, date_to=None):
         return query.order_by(Lead.scraped_at.desc()).all()
     finally:
         db.close()
+
+
+def update_lead_status(lead_id, new_status):
+    """Update a lead's status; auto-sets contacted_at and follow_up_due when moving to contacted."""
+    from db.models import Lead, LeadStatus
+    db = SessionLocal()
+    try:
+        lead = db.query(Lead).filter(Lead.id == lead_id).first()
+        if not lead:
+            return False
+        if new_status == LeadStatus.CONTACTED:
+            lead.set_contacted()
+        else:
+            lead.status = new_status
+        db.commit()
+        return True
+    finally:
+        db.close()
